10a1e080857ac6d68ec765f245c2009974dd15f5
farhan071024 <mdfarhanhaque@gmail.com>
2016-01-28 13:22:29 -0600

    4th update

--- app/src/main/java/com/farhan/haque/securityapp/Emergency.java
+++ app/src/main/java/com/farhan/haque/securityapp/Emergency.java
@@ -1,44 +1,25 @@
 package com.farhan.haque.securityapp;
 
 import android.app.Activity;
-import android.content.Intent;
-import android.net.Uri;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 
 
-public class HomeActivity extends Activity {
+public class Emergency extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
-    }
-
-    public void call(View v){
-        Uri number = Uri.parse("tel:2108711544");
-        Intent callIntent = new Intent(Intent.ACTION_CALL, number);
-        startActivity(callIntent);
-    }
-
-    public void emergency(View v){
-        Intent i = new Intent(HomeActivity.this,Emergency.class);
-        startActivity(i);
-    }
-
-    public void contactUs(View v){
-        Intent i = new Intent(HomeActivity.this,ContactUs.class);
-        startActivity(i);
+        setContentView(R.layout.activity_emergency);
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_home, menu);
+        getMenuInflater().inflate(R.menu.menu_emergency, menu);
         return true;
     }
 
