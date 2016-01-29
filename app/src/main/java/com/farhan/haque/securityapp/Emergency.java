9bbf90e2ac8dfa60db30e4d49c4a4a10ee0ecff5
farhan071024 <mdfarhanhaque@gmail.com>
2016-01-28 18:46:29 -0600

    4th update

--- app/src/main/java/com/farhan/haque/securityapp/Emergency.java
+++ app/src/main/java/com/farhan/haque/securityapp/Emergency.java
@@ -1,18 +1,41 @@
 package com.farhan.haque.securityapp;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 
 public class Emergency extends Activity {
+
+    private static final int CAMERA_REQUEST = 1888;
+    ImageView mimageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_emergency);
+
+        mimageView = (ImageView) this.findViewById(R.id.imageView);
+        Button button = (Button) this.findViewById(R.id.button4);
+    }
+
+    public void takeImageFromCamera(View view) {
+        Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(cameraIntent, CAMERA_REQUEST);
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == CAMERA_REQUEST && resultCode == RESULT_OK) {
+            Bitmap mphoto = (Bitmap) data.getExtras().get("data");
+            mimageView.setImageBitmap(mphoto);
+        }
     }
 
 
