c7dbfd2763391f9741d74c3a85fdabea2f49990b
farhan071024 <mdfarhanhaque@gmail.com>
2016-02-01 15:59:28 -0600

    Location update

--- app/src/main/java/com/farhan/haque/securityapp/Emergency.java
+++ app/src/main/java/com/farhan/haque/securityapp/Emergency.java
@@ -2,8 +2,15 @@ package com.farhan.haque.securityapp;
 
 import android.app.Activity;
 import android.content.ActivityNotFoundException;
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.location.Address;
+import android.location.Criteria;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.speech.RecognizerIntent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
@@ -18,10 +25,13 @@ import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
 
 
-public class Emergency extends Activity {
+public class Emergency extends Activity implements LocationListener {
 
     private static final int CAMERA_REQUEST = 1888;
     protected static final int RESULT_SPEECH = 1;
@@ -30,6 +40,11 @@ public class Emergency extends Activity {
     private ImageButton btnSpeak;
     private TextView txtText;
     private Spinner dropdown;
+    private LocationManager locationManager;
+    private String provider;
+    double lat, lng;
+    Geocoder geocoder;
+    static List<Address> addresses;
 
 
     @Override
@@ -45,19 +60,47 @@ public class Emergency extends Activity {
         String[] items = new String[]{"Gun", "Fight", "Fire"};
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, items);
         dropdown.setAdapter(adapter);
-    }
 
+        // Get the location manager
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        // Define the criteria how to select the locatioin provider -> use
+        // default
+        Criteria criteria = new Criteria();
+        criteria.setAccuracy(1);
+        criteria.setPowerRequirement(2);
+        criteria.isCostAllowed();
+        criteria.setCostAllowed(true);
+
+        provider = locationManager.getBestProvider(criteria, true);
+        Location location = locationManager.getLastKnownLocation(provider);
+
+        // Initialize the location fields
+        if (location != null) {
+            System.out.println("Provider " + provider + " has been selected.");
+            onLocationChanged(location);
+        } else {
+           // latituteField.setText("Location not available");
+           // longitudeField.setText("Location not available");
+            System.out.println("Provider is  " + provider );
+
+        }
+
+        locationManager.requestLocationUpdates(provider, 20000, 0, this);
+        address();
+    }
+// Takes the picture
     public void takeImageFromCamera(View view) {
         Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
         startActivityForResult(cameraIntent, CAMERA_REQUEST);
     }
 
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+// deals with camera intent
         if (requestCode == CAMERA_REQUEST && resultCode == RESULT_OK) {
             Bitmap mphoto = (Bitmap) data.getExtras().get("data");
             mimageView.setImageBitmap(mphoto);
         }
-
+// deals with voice recognition intent
         if (requestCode == RESULT_SPEECH && resultCode == RESULT_OK && null != data){
             ArrayList<String> text = data
                     .getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
@@ -82,10 +125,65 @@ public class Emergency extends Activity {
             t.show();
         }
     }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        locationManager.requestLocationUpdates(provider, 20000, 0, this);
+
+    }
+
+    /* Remove the locationlistener updates when Activity is paused */
+    @Override
+    protected void onPause() {
+        super.onPause();
+        locationManager.removeUpdates(this);
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+         lat =  location.getLatitude();
+         lng =  location.getLongitude();
+        // Toast.makeText(Emergency.this,String.valueOf(lat),Toast.LENGTH_LONG).show();
+        geocoder = new Geocoder(this, Locale.getDefault());
+
+        try {
+            addresses = geocoder.getFromLocation(lat, lng, 1); // Here 1 represent max location result to returned, by documents it recommended 1 to 5
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+        //Toast.makeText(this, "Enabled new provider " + provider,
+               // Toast.LENGTH_SHORT).show();
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+       // Toast.makeText(this, "Disabled provider " + provider,
+                //Toast.LENGTH_SHORT).show();
+    }
+
+    public void address(){
+        String address = addresses.get(0).getAddressLine(0); // If any additional address line present than only, check with max available address lines by getMaxAddressLineIndex()
+        String city = addresses.get(0).getLocality();
+        String state = addresses.get(0).getAdminArea();
+        String country = addresses.get(0).getCountryName();
+        String postalCode = addresses.get(0).getPostalCode();
+        String knownName = addresses.get(0).getFeatureName(); // Only if available else return NULL
+        Toast.makeText(Emergency.this,address+city+state+country+postalCode+knownName+String.valueOf(lat),Toast.LENGTH_LONG).show();
 
 
-
-
+    }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
