389b30c33731e9afee15613d62957213e543ee29
farhan071024 <mdfarhanhaque@gmail.com>
2016-02-12 21:02:27 -0600

    push notification update

--- app/src/main/java/com/farhan/haque/securityapp/HomeActivity.java
+++ app/src/main/java/com/farhan/haque/securityapp/HomeActivity.java
@@ -12,14 +12,23 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 
+import com.mixpanel.android.mpmetrics.MixpanelAPI;
+
 
 public class HomeActivity extends ActionBarActivity {
 
     private TextView tv1;
+    MixpanelAPI mMixpanel;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        mMixpanel = MixpanelAPI.getInstance(this, "3f84ca2c899cfb4c7ee4e8a08dd40912");
+        MixpanelAPI.People people = mMixpanel.getPeople();
+        people.identify("13793");
+        people.initPushHandling("790072665483");
+
         tv1= (TextView) findViewById(R.id.textView6);
         SharedPreferences prefs = getSharedPreferences(Example.PREFS_USER_NAME, MODE_PRIVATE);
         String name = prefs.getString("name", "No name defined");//"No name defined" is the default value.
@@ -58,7 +67,10 @@ public class HomeActivity extends ActionBarActivity {
         getMenuInflater().inflate(R.menu.menu_home, menu);
         return true;
     }
-
+    public void onDestroy() {
+        mMixpanel.flush();
+        super.onDestroy();
+    }
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
